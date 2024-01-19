989088fbdc9dc1657fa5619de661ecb991fd931f
Marcel Canhisares <marcel.canhisares@gmail.com>
2024-01-18 22:54:19 -0500

    Initial Commit

---
+++ Speedrun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speedrun
+//
+//  Created by Marcel Canhisares on 2024-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speedrun/SpeedrunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedrunApp.swift
+//  Speedrun
+//
+//  Created by Marcel Canhisares on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct SpeedrunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
